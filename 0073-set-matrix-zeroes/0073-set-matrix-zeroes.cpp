6ca2177521f27058ec025ab2a14bac47c96c28fb
iamkunal18 <118289922+iamkunal18@users.noreply.github.com>
2023-05-08 00:20:29 +0530

    Time: 14 ms (72.64%), Space: 13.2 MB (91.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,21 +4,33 @@ public:
         int size1 = matrix.size();
         int size2 = matrix[0].size();
         
-        vector<int> v1(size2, 0);
-        vector<int> v2(size1, 0);
+        int strtCol = 1; 
+        
         for(int i = 0; i < size1; i++) {
            for(int j = 0; j < size2; j++) {
                if(matrix[i][j] == 0) {
-                   v1[j] = 1;
-                   v2[i] = 1;
+                   if(j == 0){
+                       strtCol = 0;
+                   }
+                   else{
+                       matrix[i][0] = 0;
+                       matrix[0][j] = 0;
+                   }
                }
            } 
         }
-        for(int i = 0; i < size1; i++) {
-           for(int j = 0; j < size2; j++){ 
-               if(v1[j] == 1) matrix[i][j] = 0;
-               if(v2[i] == 1) matrix[i][j] = 0;
+        for(int i = size1-1; i > 0; i--) {
+           for(int j = size2-1; j > 0; j--){ 
+               if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
            }
         }
+        
+        for(int i = 1; i < size2; i++) {
+            if(matrix[0][0] == 0) matrix[0][i] = 0;
+        }
+        for(int j = 0; j < size1; j++) {
+            if(strtCol == 0) matrix[j][0] = 0; 
+        }
+        
     }
 };
