e14a1a5ec4498fa999a9d4e8e194bd15266578ba
iamkunal18 <118289922+iamkunal18@users.noreply.github.com>
2023-05-08 13:38:32 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (16.21%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+    vector<int> generaterow(int rowNo) {
+        vector<int> row;
+        int temp_variable = 1;
+        row.push_back(temp_variable);
+
+        for(int col = 1; col < rowNo; col++) {
+            // Using concept of nCr
+            temp_variable = temp_variable *  (rowNo-col);
+            temp_variable = temp_variable / col;
+            row.push_back(temp_variable);
+        }
+        return row;
+        
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        vector<int> temp(1, 1);
+        vector<int> firstRow(1, 1);
                 
-        ans.push_back(temp);
+        ans.push_back(firstRow);
         
-        for(int i = 2; i <= numRows; i++) {
-            vector<int> temp2;
-            int temp_variable = 1;
-            temp2.push_back(temp_variable);
-            
-            for(int j = 1; j < i; j++) {
-                // Using concept of nCr
-                temp_variable = temp_variable *  (i-j);
-                temp_variable = temp_variable / j;
-                temp2.push_back(temp_variable);
-            }
-            
-            ans.push_back(temp2);            
+        for(int row = 2; row <= numRows; row++) {
+            ans.push_back(generaterow(row));            
         }
         
         return ans;
