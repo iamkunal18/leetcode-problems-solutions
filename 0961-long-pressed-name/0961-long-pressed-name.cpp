ca5738a94e9fcf140ad74eccfc4319d3fe8bde6a
iamkunal18 <118289922+iamkunal18@users.noreply.github.com>
2023-09-29 18:45:47 +0530

    Time: 4 ms (7.74%), Space: 6.7 MB (19.62%) - LeetHub

--- 0961-long-pressed-name/0961-long-pressed-name.cpp
+++ 0961-long-pressed-name/0961-long-pressed-name.cpp
@@ -1,30 +1,33 @@
-class Solution {
-public:
-    bool isLongPressedName(string name, string typed) {
-        bool ans = true;
-
-        int j=0;
-        int i=0;
-        if(name[i] != typed[j]) return false;
-        while(i<=name.size()) {
-            if(j>=typed.size()) break;
-            if(name[i]==typed[j]) {
-                i++;
-                j++;
-            } else if(i!=0 && name[i-1] == typed[j]) {
-                j++;
-            } else if(name[i]!=typed[j] && (i!=0 && name[i-1] != typed[j])) {
-                cout<<"here"<<endl;
-                ans=false;
-                break;
+class Solution
+{
+    public:
+        bool isLongPressedName(string name, string typed)
+        {
+            if (typed.size() < name.size())
+            {
+                return false;
             }
-        }
 
-        if(i<name.size() || j<typed.size()) {
-            cout<<"here2"<<endl;
-            ans = false;
-        }
+            int i = 0;
+            int j = 0;
 
-        return ans;
-    }
+            while (j < typed.size())
+            {
+                if (i < name.size() && (name[i] == typed[j]))
+                {
+                    i++;
+                    j++;
+                }
+                else if (j > 0 && typed[j] == typed[j - 1])
+                {
+                    j++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return i == name.size();
+        }
 };
